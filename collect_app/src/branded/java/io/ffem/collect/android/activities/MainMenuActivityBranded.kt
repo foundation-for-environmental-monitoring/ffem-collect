24da45879203f6c2e8c806a9ce07b101a527a1f9
nhternup <nhternup@users.noreply.github.com>
2021-01-26 23:43:02 +0530

    Merge odk changes

--- collect_app/src/branded/java/io/ffem/collect/android/activities/MainMenuActivityBranded.kt
+++ collect_app/src/branded/java/io/ffem/collect/android/activities/MainMenuActivityBranded.kt
@@ -8,32 +8,18 @@ import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.MenuItem
-import android.widget.TextView
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.ffem.collect.android.util.ApkHelper.isNonStoreVersion
 import org.odk.collect.android.BuildConfig
 import org.odk.collect.android.R
-import org.odk.collect.android.activities.DeleteSavedFormActivity
 import org.odk.collect.android.activities.FormDownloadListActivity
 import org.odk.collect.android.analytics.Analytics
 import org.odk.collect.android.application.Collect
 import org.odk.collect.android.configure.SettingsImporter
-import org.odk.collect.android.configure.qr.QRCodeActivityResultDelegate
 import org.odk.collect.android.configure.qr.QRCodeDecoder
-import org.odk.collect.android.configure.qr.QRCodeMenuDelegate
-import org.odk.collect.android.configure.qr.QRCodeTabsActivity
 import org.odk.collect.android.gdrive.GoogleDriveActivity
-import org.odk.collect.android.logic.PropertyManager
-import org.odk.collect.android.logic.PropertyManager.PROPMGR_EMAIL
-import org.odk.collect.android.logic.PropertyManager.PROPMGR_USERNAME
 import org.odk.collect.android.preferences.GeneralKeys
-import org.odk.collect.android.preferences.PreferencesActivity
 import org.odk.collect.android.storage.StorageInitializer
 import org.odk.collect.android.storage.StorageStateProvider
 import org.odk.collect.android.utilities.MultiClickGuard
@@ -57,10 +43,6 @@ open class MainMenuActivityBranded : AppUpdateActivity() {
     lateinit var analytics: Analytics
 
     private var toolbar: Toolbar? = null
-    private lateinit var propertyManager: PropertyManager
-    private var usernameTextView: TextView? = null
-    private var emailTextView: TextView? = null
-    private lateinit var activityResultDelegate: QRCodeActivityResultDelegate
 
     private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -86,9 +68,6 @@ open class MainMenuActivityBranded : AppUpdateActivity() {
                 broadcastReceiver,
                 IntentFilter("DOWNLOAD_FORMS_ACTION")
         )
-
-        activityResultDelegate = QRCodeActivityResultDelegate(this, settingsImporter, qrCodeDecoder, analytics)
-
     }
 
     private fun hasAppVersionExpired(): Boolean {
@@ -138,26 +117,6 @@ open class MainMenuActivityBranded : AppUpdateActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    fun onSettingsClick(@Suppress("UNUSED_PARAMETER") item: MenuItem) {
-        item.isChecked = false
-        startActivity(Intent(this, PreferencesActivity::class.java))
-    }
-
-    fun onDeleteClick(@Suppress("UNUSED_PARAMETER") item: MenuItem) {
-        val i = Intent(applicationContext, DeleteSavedFormActivity::class.java)
-        startActivity(i)
-    }
-
-    private fun openUrl(url: String, context: Context) {
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        ContextCompat.startActivity(context, browserIntent, null)
-    }
-
-    fun onAboutClick(@Suppress("UNUSED_PARAMETER") item: MenuItem) {
-        item.isChecked = false
-        startActivity(Intent(this, AboutActivity::class.java))
-    }
-
     fun getBlankForm() {
         if (MultiClickGuard.allowClick(javaClass.name)) {
             val sharedPreferences = PreferenceManager
@@ -179,10 +138,5 @@ open class MainMenuActivityBranded : AppUpdateActivity() {
             }
             startActivity(i)
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        activityResultDelegate.onActivityResult(requestCode, resultCode, data)
     }
 }
